d14ef48aeca41321fe9cb40423f55b286266e3ad
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2018-07-30 02:43:21 +0300

    new cattish page

--- test.py
+++ test.py
@@ -11,71 +11,34 @@ import re
 import traceback
 
 HTTP_PATH = 'www'
-PORT = 5000
 
 class IndexHandler(RequestHandler):
     def get(self):
-        #f = open(HTTP_PATH + '/index.html', mode='rb')
-        #self.write(f.read())
-        #f.close()
-        self.render(HTTP_PATH + '/index.html', PORT=PORT);
+        #self.render(HTTP_PATH + '/index.html');
+        f = open(HTTP_PATH + '/index.html', mode='rb')
+        self.write(f.read())
+        f.close()
 
     def post(self):
-        self.write('Client with ip <%s> sent <%s>' % (self.request.remote_ip, self.request.body))
-
-
-class WSHandler(WebSocketHandler):
-    def check_origin(self, origin):
-        return True
-
-    def open(self, *args):
-        self.stream.set_nodelay(True)
-        ip = self.request.remote_ip
-        user_id = -1
-        user_hash = -1
-        try:
-            user_id = int(self.get_argument('id'))
-            user_hash = int(self.get_argument('hash'))
-        except:
-            pass
-        self.write_message('{"id":%s,"hash":%s,"ip":%s}' % 
-            (user_id, user_hash, ip), binary=False)
-        print('WS OPENED, IP: %s' % ip)
-
-    def on_message(self, message):  
-        print('WS GETS MESSAGE <%s>' % message)
-        
-
-    def on_close(self):
-        print('WS CLOSED')
-
+        print('Client with ip <%s> sent <%s>' % (self.request.remote_ip, self.request.body))
 
 
 def main():
-    global PORT
-    PORT = int(os.environ.get("PORT", PORT))
-    
-    print('Server runs on port %s' % PORT)
-
+    port = int(os.environ.get("PORT", 5000))
+    print('Server runs on port %s' % port)
     app = Application([
-        (r'/ws', WSHandler),
         (r'/', IndexHandler),
         (r'/(.*)', StaticFileHandler, {"path": HTTP_PATH}),
     ],
         autoreload=True
     )
-
-    app.listen(PORT)
-    
+    app.listen(port)
     try:
         IOLoop.instance().start()  
     except KeyboardInterrupt:
         pass
-
     IOLoop.instance().stop()
-    
     print('Server shutted down')
-
     time.sleep(1)
 
 if '__main__' == __name__:
