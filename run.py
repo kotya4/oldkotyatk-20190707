4d90a9e47c936fad0940874ebed2f322a120ad93
Sluchaynaya Kotya <sluchaynayakotya@gmail.com>
2018-08-29 20:57:21 +0300

    http server test

--- run.py
+++ run.py
@@ -24,14 +24,14 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
 
 def main():
-"""
+    """
     bot = Bot(token='646357441:AAGOwonTs3T9fZfN8KxCE22FMxtA-bWYhPI')
     dp = Dispatcher(bot)
     @dp.message_handler(commands=['start', 'help'])
     async def send_welcome(message: types.Message):
         await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
     executor.start_polling(dp)
-"""
+    """
     port = int(os.environ.get("PORT", 8081))
     server_address = ('', port)
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
